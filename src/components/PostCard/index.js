696d93a15fb9fc35be8f767549540ac3170af44e
Salvador Gonzalez <salvadorgonmo@gmail.com>
2021-07-22 15:08:03 -0500

    refactors imports

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
+import {
+  Card, CardHeader, CardContent, CardActions, IconButton, Typography,
+} from '@material-ui/core';
 import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
